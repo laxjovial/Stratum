cfd33701a4992fe26057bfa1408cb155a9414965
laxjovial <laxjovial@gmail.com>
2025-08-09 13:11:01 +0100

    Update firebase.ts

--- packages/frontend/src/lib/firebase.ts
+++ packages/frontend/src/lib/firebase.ts
@@ -1,6 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
+// The getAnalytics function is not available in all environments, so we need to
+// check if the window object exists.
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -8,10 +13,12 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase for client side
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
+// const analytics = getAnalytics(app);
 
 export { app, auth };
